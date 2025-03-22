2d6674c64e6641eb2b58a5bd975a0d18dcefed17
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-03-22 10:53:43 +0530

    Create FlappyBird.java

---
+++ src/FlappyBird.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;     //for storing pipes
+import java.util.Random;        //For placing pipes at random place
+import javax.swing.*;
+
+public class FlappyBird extends JPanel{
+  
+}
