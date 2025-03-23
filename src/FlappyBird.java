12e6ba5dd49e40bc74e128733a001b6f05ec64c5
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-03-23 11:07:28 +0530

    Update FlappyBird.java

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -5,5 +5,12 @@ import java.util.Random;        //For placing pipes at random place
 import javax.swing.*;
 
 public class FlappyBird extends JPanel{
-  
+    int boardWidth = 360;
+    int boardHeight = 640;
+
+    FlappyBird(){
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.blue);
+
+    }
 }
