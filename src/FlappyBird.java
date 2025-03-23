055190eea1494af460ccfce9d6e4202fc2059a61
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-03-23 11:08:02 +0530

    Update FlappyBird.java

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -16,12 +16,23 @@ public class FlappyBird extends JPanel{
 
     FlappyBird(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
-        setBackground(Color.blue);
+//        setBackground(Color.blue);
 
         //load Images
         backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
         birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
         topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
         bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g){
+        //background
+        g.drawImage(backgroundImg, 0 , 0, boardWidth, boardHeight, null);
+
     }
 }
