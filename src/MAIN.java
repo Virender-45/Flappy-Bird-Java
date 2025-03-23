9cfd648892426bb1033b26fba358224d0f812417
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-03-23 11:07:12 +0530

    Update MAIN.java

--- src/MAIN.java
+++ src/MAIN.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.util.zip.DeflaterInputStream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -6,11 +7,16 @@ public class Main {
         int boardHeight = 640;
 
         JFrame frame = new JFrame("Flappy Bird");
-        frame.setVisible(true);
-        frame.setSize(boardWidth, boardHeight);
+//        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);     //These are the dimensions of our Background image
         frame.setLocationRelativeTo(null);  //Place window in centre
         frame.setResizable(false);  //User cannot resize the window
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //Exit only when user click on close
 
+        //Add JPanel onto frame
+        FlappyBird FlappyBird = new FlappyBird();
+        frame.add(FlappyBird);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
