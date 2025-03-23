67a8dd8d845a8d3c0ccee590c5b817506cfba8ea
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-03-23 11:08:37 +0530

    Update FlappyBird.java

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -14,6 +14,28 @@ public class FlappyBird extends JPanel{
     Image topPipeImg;
     Image bottomPipeImg;
 
+    //Bird
+    int birdX = boardWidth/8;
+    int birdY = boardHeight/2;
+    int birdWidth = 34;
+    int birdHeight = 24;
+
+    class Bird{
+        int x = birdX;
+        int y = birdY;
+        int width = birdWidth;
+        int height = birdHeight;
+        Image img;
+
+        Bird(Image img){    //Constructor
+            this.img = img;
+        }
+    }
+
+    //game logic
+    Bird bird;
+
+
     FlappyBird(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
 //        setBackground(Color.blue);
@@ -23,6 +45,9 @@ public class FlappyBird extends JPanel{
         birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
         topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
         bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+
+        //bird
+        bird = new Bird(birdImg);
     }
 
     public void paintComponent(Graphics g){
@@ -34,5 +59,7 @@ public class FlappyBird extends JPanel{
         //background
         g.drawImage(backgroundImg, 0 , 0, boardWidth, boardHeight, null);
 
+        //bird
+        g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
     }
 }
